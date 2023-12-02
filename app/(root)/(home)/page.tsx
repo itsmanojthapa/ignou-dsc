5dd78b4d167064c991ad40a07976bb73e03f4e10
itsmanojthapa <itsmanojthapa@gmail.com>
2023-12-02 17:20:17 +0530

    Clerk/nextjs init

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="h1-bold dark-gradient">Hello World</h1>
-        <h2 className="h2-bold">Hello World</h2>
-        <h3 className="h3-bold">Hello World</h3>
+        <UserButton afterSignOutUrl="/" />
+        <h1 className="h1-bold ">Hello World</h1>
       </div>
     </main>
   );
