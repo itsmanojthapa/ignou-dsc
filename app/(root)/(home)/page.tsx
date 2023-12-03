5f5a64e5b93b78314b60df449749f611b240298f
itsmanojthapa <itsmanojthapa@gmail.com>
2023-12-03 12:15:50 +0530

    🖥️  shadcn/ui installed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,17 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Link } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1 className="h1-bold ">HOME</h1>
+        <Button variant="outline" asChild>
+          <div>
+            Hello World
+            <Link href="/login">Click here</Link>
+          </div>
+        </Button>
       </div>
     </main>
   );
